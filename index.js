5e78b73e83d455fb72e2cdb18d5f3df29b39b76e
Mélanie FAURE <melanie@mbp-de-melanie.lan>
2021-11-29 15:14:15 +0100

    change syntax of mongoose.connect

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const formidable = require("express-formidable");
 const mongoose = require("mongoose");
 const cloudinary = require("cloudinary").v2;
 
-mongoose.connect("process.env.MONGODB_URI)");
+mongoose.connect(process.env.MONGODB_URI);
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_API_NAME,
@@ -26,6 +26,6 @@ app.get("/", (req, res) => {
   res.send("Welcome to my vinted api");
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server has started");
 });
